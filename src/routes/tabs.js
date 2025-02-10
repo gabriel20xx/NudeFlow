97af0324521cdf796a1ee0f0078464e4a7707699
Gabriel20xx <gabrielfranz31@gmail.com>
2025-02-10 16:44:19 +0100

    update

---
+++ src/routes/tabs.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.use(express.static("public")); // For serving static files like CSS, JS
+router.get("/", (req, res) => res.render("home")); // Default route
+
+router.get("/:page", (req, res) => {
+  res.render("layout", { page: req.params.page }, (err, html) => {
+    if (err) return res.status(404).send("<h2>Page Not Found</h2>");
+    res.send(html);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -8,18 +8,21 @@ const fs = require("fs");
 
 const port = 5000;
 
+const staticPath = path.join(__dirname, "public");
+const modelsPath = path.join(__dirname, "../../mnt/models");
+const imagesPath = path.join(__dirname, "../../mnt/models");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const imagesRoutes = require('./routes/images');
+app.use('/images', imagesRoutes);
 
 mongoose
   .connect("mongodb://192.168.2.94:27017/xxxtok")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Failed to connect to MongoDB", err));
-
-const staticPath = path.join(__dirname, "public");
-const modelsPath = path.join(__dirname, "../../mnt/models");
-const imagesPath = path.join(__dirname, "../../mnt/images");
 
 // Read the filenames in the directory
 fs.readdirSync(modelsPath).forEach((file) => {
@@ -31,15 +34,6 @@ fs.readdirSync(modelsPath).forEach((file) => {
 });
 
 app.set("view engine", "ejs");
-app.use(express.static("public")); // For serving static files like CSS, JS
-app.get("/", (req, res) => res.render("home")); // Default route
-
-app.get("/:page", (req, res) => {
-  res.render("layout", { page: req.params.page }, (err, html) => {
-    if (err) return res.status(404).send("<h2>Page Not Found</h2>");
-    res.send(html);
-  });
-});
 
 // Endpoint to fetch available route names dynamically
 app.get("/api/routes", (req, res) => {
@@ -54,67 +48,6 @@ app.get("/api/routes", (req, res) => {
 
   res.json(routes); // Send the list of routes as JSON
 });
-
-// Route to serve a random WebP image
-app.get("/media/homepage", (req, res) => {
-  try {
-    const images = getAllWebPImages(imagesPath);
-
-    if (images.length === 0) {
-      return res.status(404).send("No images found");
-    }
-
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    const fileData = fs.readFileSync(randomImage);
-
-    res.set("Content-Type", "image/webp");
-    res.send(fileData);
-  } catch (err) {
-    console.error("Error accessing images:", err);
-    res.status(500).send("Internal server error");
-  }
-});
-
-// Route to serve a specific WebP image
-app.get("/media/:category", async (req, res) => {
-  try {
-    const category = req.params.category;
-    const categoryPath = path.join(imagesPath, category); // Path to the file on the SMB share
-    const images = getAllWebPImages(categoryPath);
-
-    if (images.length === 0) {
-      return res.status(404).send("No images found");
-    }
-
-    const randomImage = images[Math.floor(Math.random() * images.length)];
-    const fileData = fs.readFileSync(randomImage);
-
-    res.set("Content-Type", "image/webp");
-    res.send(fileData);
-  } catch (err) {
-    console.error("Error accessing images:", err);
-    res.status(500).send("Internal server error");
-  }
-});
-
-// Function to get all WebP image file paths from a directory and subdirectories
-const getAllWebPImages = (dir) => {
-  let results = [];
-  const files = fs.readdirSync(dir);
-
-  for (const file of files) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-
-    if (stat && stat.isDirectory()) {
-      results = results.concat(getAllWebPImages(fullPath));
-    } else if (file.endsWith(".webp")) {
-      results.push(fullPath);
-    }
-  }
-
-  return results;
-};
 
 // Start the server
 app.listen(port, () => {
