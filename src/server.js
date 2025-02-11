a8049168c8cd357684b36593a0cc57b74686189b
Gabriel <gabrielfranz31@gmail.com>
2025-02-11 09:32:49 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "Home Page" });
+  res.render("index", { title: "Home Page" });
 });
 
 const imagesRoutes = require("./routes/images");
