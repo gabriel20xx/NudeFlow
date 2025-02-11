e3646d5d9b42552c3c02d9e7a0c2b64e122bceb7
Gabriel <gabrielfranz31@gmail.com>
2025-02-11 09:32:28 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", { title: "Home Page" });
