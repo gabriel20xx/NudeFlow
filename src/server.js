6ed29f0ec0dd459c8695fdc625264270a5b79105
Gabriel <gabrielfranz31@gmail.com>
2025-02-11 14:34:42 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); 
 app.use(express.static(path.join(__dirname, "public")));  
 
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
 const imagesRoutes = require("./routes/images");
 const tabsRoutes = require("./routes/tabs");
 const { setupDynamicRoutes } = require("./controllers/dynamicRoutesController");
