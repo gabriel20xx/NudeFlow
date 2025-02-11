84b9d9ac12a336608777286d9a7e936997022589
Gabriel <gabrielfranz31@gmail.com>
2025-02-11 17:33:01 +0100

    Update tabs.js

--- src/routes/tabs.js
+++ src/routes/tabs.js
@@ -3,8 +3,14 @@ const path = require("path");
 const router = express.Router();
 
 router.use(express.static(path.join(__dirname, '../public'))); // For serving static files like CSS, JS
-router.get("/", (req, res) => res.render("home")); // Default route
-router.get("/categories", (req, res) => res.render("categories"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
+
+app.get("/categories", (req, res) => {
+  res.render("categories", { title: "Categories" });
+});
 
 router.get("/:page", (req, res) => {
   res.render("partials", { page: req.params.page }, (err, html) => {
