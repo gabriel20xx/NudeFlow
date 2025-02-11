6eacc2f11878551aea1a9b897b41e0a8ca854b09
Gabriel <gabrielfranz31@gmail.com>
2025-02-11 14:57:01 +0100

    Update routesApi.js

---
+++ src/api/routesApi.js
@@ -0,0 +1,21 @@
+// /my-app/src/routes/routes.js
+const fs = require("fs");
+const path = require("path");
+
+const modelsPath = path.join(__dirname, "../models"); // Adjust path if necessary
+
+// Endpoint to fetch available route names dynamically
+module.exports = function(app) {
+  app.get("/api/routes", (req, res) => {
+    const routes = [];
+
+    // Read the filenames in the models directory and create route names
+    fs.readdirSync(modelsPath).forEach((file) => {
+      const route = path.basename(file, path.extname(file)); // Strip extension to get the route
+      routes.push(route);
+    });
+    console.log(routes);
+
+    res.json(routes); // Send the list of routes as JSON
+  });
+};
