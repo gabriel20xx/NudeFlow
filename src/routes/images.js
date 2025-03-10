09ebb7f04a6ff62222d4abb600031bfe07c4012f
Gabriel <gabrielfranz31@gmail.com>
2025-03-10 21:37:36 +0100

    Update images.js

--- src/routes/images.js
+++ src/routes/images.js
@@ -4,24 +4,24 @@ const path = require("path");
 const fs = require("fs");
 const router = express.Router();
 
-const contentPath = path.join(__dirname, "../../../mnt/images");
+const mediaPath = path.join(__dirname, "../../../mnt/images");
 
 // Route to serve a random WebP image
 router.get("/homepage", (req, res) => {
   try {
-    const content = getAllContent(contentPath);
+    const media = getAllMedia(mediaPath);
 
-    if (images.length === 0) {
+    if (media.length === 0) {
       return res.status(404).send("No images found");
     }
 
-    const randomContent = content[Math.floor(Math.random() * content.length)];
-    const fileData = fs.readFileSync(randomContent);
+    const randomMedia = media[Math.floor(Math.random() * media.length)];
+    const fileData = fs.readFileSync(randomMedia);
 
     res.set("Content-Type", "video/mp4");
     res.send(fileData);
   } catch (err) {
-    console.error("Error accessing content:", err);
+    console.error("Error accessing media:", err);
     res.status(500).send("Internal server error");
   }
 });
@@ -30,26 +30,26 @@ router.get("/homepage", (req, res) => {
 router.get("/:category", async (req, res) => {
   try {
     const category = req.params.category;
-    const categoryPath = path.join(contentPath, category); // Path to the file on the SMB share
-    const content = getAllContent(categoryPath);
+    const categoryPath = path.join(mediaPath, category); // Path to the file on the SMB share
+    const media = getAllMedia(categoryPath);
 
-    if (content.length === 0) {
-      return res.status(404).send("No images found");
+    if (media.length === 0) {
+      return res.status(404).send("No media found");
     }
 
-    const randomContent = content[Math.floor(Math.random() * content.length)];
+    const randomMedia = media[Math.floor(Math.random() * media.length)];
     const fileData = fs.readFileSync(randomContent);
 
     res.set("Content-Type", "video/mp4");
     res.send(fileData);
   } catch (err) {
-    console.error("Error accessing content:", err);
+    console.error("Error accessing media:", err);
     res.status(500).send("Internal server error");
   }
 });
 
 // Function to get all WebP image file paths from a directory and subdirectories
-const getAllContent = (dir) => {
+const getAllMedia = (dir) => {
   let results = [];
   const files = fs.readdirSync(dir);
 
@@ -58,7 +58,7 @@ const getAllContent = (dir) => {
     const stat = fs.statSync(fullPath);
 
     if (stat && stat.isDirectory()) {
-      results = results.concat(getAllContent(fullPath));
+      results = results.concat(getAllMedia(fullPath));
     } else if (file.endsWith("-audio.mp4")) {
       results.push(fullPath);
     }
